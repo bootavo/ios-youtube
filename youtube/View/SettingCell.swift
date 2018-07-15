bcbb47aa660bb0a1a43c583940fbd17f72cc9b8a
Gustavo Tufiño Fernández <tavo.tf@gmail.com>
2018-07-14 20:44:18 -0500

    Add SettingCell on SettingLauncher

---
+++ youtube/View/SettingCell.swift
@@ -0,0 +1,62 @@
+//
+//  SettingCell.swift
+//  youtube
+//
+//  Created by Gustavo Tufiño Fernandez on 7/14/18.
+//  Copyright © 2018 GAP GLOBAL SOLUTIONS S.A.C. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: BaseCell {
+    
+    override var isHighlighted: Bool {
+        didSet{
+            backgroundColor = isHighlighted ? UIColor.darkGray : UIColor.white
+            
+            nameLabel.textColor = isHighlighted ? UIColor.white : UIColor.black
+            
+            iconImageView.tintColor = isHighlighted ? UIColor.white : UIColor.darkGray
+        }
+    }
+    
+    var setting: Setting? {
+        didSet{
+            nameLabel.text = setting?.name
+            if let imageName = setting?.imageName {
+                iconImageView.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+                iconImageView.tintColor = UIColor.darkGray
+            }
+            
+        }
+    }
+    
+    let nameLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Settings"
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    let iconImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "settings")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(25)]-8-[v1]|", views: iconImageView, nameLabel)
+        
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstraintsWithFormat(format: "V:|-12-[v0(25)]-12-|", views: iconImageView)
+        addConstraint(NSLayoutConstraint(item: self.iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self.iconImageView, attribute: .centerY, multiplier: 1, constant: 0))
+        
+    }
+    
+}
---
+++ youtube/View/SettingsLauncher.swift
@@ -0,0 +1,124 @@
+//
+//  SettingsLauncher.swift
+//  youtube
+//
+//  Created by Gustavo Tufiño Fernandez on 7/14/18.
+//  Copyright © 2018 GAP GLOBAL SOLUTIONS S.A.C. All rights reserved.
+//
+
+import UIKit
+
+class Setting: NSObject {
+    
+    var name:String?
+    var imageName:String?
+    
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    let cellHeight: CGFloat = 50
+    
+    let settings: [Setting] = {
+        var settings = Setting()
+        settings.name = "Settings"
+        settings.imageName = "settings"
+        
+        var terms = Setting()
+        terms.name = "Terms & privacy policy"
+        terms.imageName = "settings"
+        
+        var feedback = Setting()
+        feedback.name = "Send Feedback"
+        feedback.imageName = "settings"
+        
+        var help = Setting()
+        help.name = "Help"
+        help.imageName = "settings"
+        
+        var switch_account = Setting()
+        switch_account.name = "Switch Account"
+        switch_account.imageName = "settings"
+        
+        var cancel = Setting()
+        cancel.name = "Cancel"
+        cancel.imageName = "settings"
+        
+        return [settings, terms, feedback, help, switch_account, cancel]
+        
+    }()
+        
+    override init() {
+        super.init()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+        
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    let blackView = UIView()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
+    
+    func showSettings(){
+        if let window = UIApplication.shared.keyWindow {
+            blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+            
+            window.addSubview(blackView)
+            window.addSubview(collectionView)
+            
+            let height: CGFloat = CGFloat(settings.count) * cellHeight
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
+            blackView.frame = window.frame
+            blackView.alpha = 0
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.blackView.alpha = 1
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }, completion: nil)
+            
+        }
+    }
+    
+    @objc func handleDismiss(){
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow{
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
+            
+        }
+    }
+    
+}
